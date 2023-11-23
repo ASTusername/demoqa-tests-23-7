e1c49443be2633ab31cfe879a7cc6c20f7e2c4ef
Alexandr Trifonov <ast89646421790@yandex.ru>
2023-11-23 12:24:19 +0300

    add homeworkfaker

--- src/test/java/tests/constants/DataTest.java
+++ src/test/java/tests/constants/DataTest.java
@@ -1,22 +1,28 @@
 package tests.constants;
 
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+import static utils.RandomUtils.getRandomCity;
+
 public class DataTest {
-    public static final String FIRST_NAME = "Alex";
-    public static final String LAST_NAME = "Surname Alex";
-    public static final String USER_EMAIL = "222@name.ru";
-    public static final String GENDER = "Male";
-    public static final String USER_NUMBER = "1234567890";
-    public static final String USER_NUMBER_NOT_VALID = "1";
-    public static final String DAY_OF_BIRTH = "17";
-    public static final String MONTH_OF_BIRTH = "December";
-    public static final String YEAR_OF_BIRTH = "1977";
-    public static final String SUBJECTS1 = "History";
-    public static final String SUBJECTS2 = "English";
-    public static final String HOBBIES1 = "Sports";
-    public static final String HOBBIES2 = "Reading";
-    public static final String UPLOAD_PICTURE = "img/1.png";
-    public static final String CURRENT_ADDRESS = "My present address";
-    public static final String STATE = "NCR";
-    public static final String CITY = "Delhi";
+    static Faker faker = new Faker(new Locale("en-GB"));
+    public static final String FIRST_NAME = faker.name().firstName();
+    public static final String LAST_NAME = faker.name().lastName();
+    public static final String USER_EMAIL = faker.internet().emailAddress();
+    public static final String GENDER = faker.options().option("Male", "Female", "Other");
+    public static final String USER_NUMBER = faker.phoneNumber().subscriberNumber(10);
+    public static final String USER_NUMBER_NOT_VALID = faker.number().digits(1);
+    public static final String DAY_OF_BIRTH = String.valueOf(faker.number().numberBetween(1, 28)); // как быть с днем 30?
+    public static final String MONTH_OF_BIRTH = faker.options().option("January", "March", "April", "May",
+            "June", "July", "August", "September", "October", "November", "December");
+    public static final String YEAR_OF_BIRTH = String.valueOf(faker.number().numberBetween(1900, 2022));
+    public static final String SUBJECTS = faker.options().option("History", "English");
+    public static final String HOBBIES = faker.options().option("Sports", "Reading");
+    public static final String UPLOAD_PICTURE = String.format("img/%s.png", faker.number().numberBetween(1, 2));
+    public static final String CURRENT_ADDRESS = faker.address().fullAddress();
+    public static final String STATE = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
+    public static final String CITY = getRandomCity(STATE);
 
 }
